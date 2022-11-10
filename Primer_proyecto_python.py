043b81fab291892fb901f7415e24ac8e5600b9ad
GLuchian <94Egroeg@gmail.com>
2022-11-10 11:52:06 +0100

    Update xd

--- Primer_proyecto_python.py
+++ Primer_proyecto_python.py
@@ -16,7 +16,7 @@ print(" ")
 print(penguins.shape) 
 print("Número de observaciones: ", penguins.shape[0],";  Número de caracteristicas: ", penguins.shape[1])
 print(" ")
-print(penguins.info())
+print(penguins.info()) #El comando info() nos ofrece el tipo de datos de cada columna
 print(" ")
 
 
@@ -31,7 +31,7 @@ penguins.dropna(inplace=True)
 print(" ")
 print("Utilizando el comando dropna(), eliminamos todas las filas que contengan algún NA.")
 print(" ")
-print(penguins.isna().sum())
+print(penguins.isna().sum()) #Sumamos todos los NA de cada columna
 print(" ")
 
 
@@ -41,22 +41,23 @@ print("del pico según el sexo:")
 print(" ")
 print("Agrupamos por la columna 'sex' y contamos el numero de individuos que hay.")
 print(" ")
-count_sex = penguins.groupby('sex')["sex"].count()
+count_sex = penguins.groupby('sex')["sex"].count() #Agrupamos por la columna 'sex' y contamos cuantos individuos hay
 print(count_sex)
 print(" ")
 
 print("Agrupamos por la columna 'sex' y mediante el comando aggregate() hacemos la media del pico.")
 bill_bysex=penguins.groupby("sex").aggregate({
-    "bill_length_mm":np.mean})
+    "bill_length_mm":np.mean}) #Agrupamos por la columna 'sex' y con el comando agregate() hacemos la media de la columna 'bill_length_mm'
 print(" ")
 print(bill_bysex)
 print(" ")
 print("Otra manera de hacerlo es mediante el comando discribe(), localizando las columnas")
 print("que nos interesan que son 'count' y 'mean'.")
-#Mediante esta única linea de código podemos sacar por pantalla los dos resultados anteriores.
-#Como el resultado que sacamos con el método describe() es una tabla multiíndice,
-#primero filtraremos por la columna "bill_length_mm" y dentro de esta columna, volvemos a
-#filtrar por las columnas que nos interesa que son "count" y "mean"
+
+#Mediante esta única linea de código, también podemos sacar por pantalla los dos resultados anteriores.
+#Como el resultado que sacamos con el método describe() es una tabla multiíndice, primero filtraremos 
+#por la columna "bill_length_mm" y dentro de esta columna, volvemos a filtrar por las columnas
+#que nos interesa que son "count" y "mean"
 bill_cm_bysex = penguins.groupby('sex').describe().loc[:,"bill_length_mm"].loc[:,("count", "mean")]
 print(" ")
 print(bill_cm_bysex)
@@ -82,11 +83,12 @@ print("Hagamos algo un poco más elaborado, vamos a realizar una agrupación en 
 print("de cada observación. Queremos obtener solamente la información referente al sexo Femenino.")
 #
 
-gr_sex_sp = penguins.groupby(["sex","species"]).describe()
+gr_sex_sp = penguins.groupby(["sex","species"]).describe() #Agrupamos por las columnas 'sex' y 'species' 
+                                                           #y utilizamos describe() para obtener información
 print(" ")
 print("Agrupamos por 'sex' y 'species'")
 print(" ")
-print(gr_sex_sp) #Obtenemos información agrupando la columna de "sex" y "species"
+print(gr_sex_sp)
 print(" ")
 print("Capturamos solo la información relativa al sexo femenino")
 print(" ")
@@ -100,10 +102,14 @@ print(" ")
 print(penguins)
 print(" ")
 print("Creamos la columna body_mass_kg y eliminamos la columna body_mass_g")
-body_mass_kg=penguins["body_mass_g"]/1000
-penguins["body_mass_kg"]=body_mass_kg
 
-penguins.drop(["body_mass_g"], axis=1, inplace=True) #Eliminamos la columna "body_mass_g"
+body_mass_kg=penguins["body_mass_g"]/1000 #Creamos la columna 'body_mass_kg'
+penguins["body_mass_kg"]=body_mass_kg #Añadimos esta columna al dataframe
+
+penguins.drop(["body_mass_g"], axis=1, inplace=True) #Eliminamos la columna "body_mass_g" utilizando axis=1 
+                                                     #para eliminar una columna y no una fila e inplace=True 
+                                                     #para que no nos devuelva una copia sino que el cambio 
+                                                     #tenga efecto sobre el dataframe.                                                    
 print(" ")
 print(penguins)
 
